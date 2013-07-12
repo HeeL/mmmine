44a2d4de9fe832b5f4c2b748d623078915979758
HeeL <parizhskiy@gmail.com>
2013-07-12 23:34:50 +0200

    refactored assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree ./lib
-//= require main
-//= require users
-//= require products
-//= require comments
-//= require more
+//= require_tree
+//= require settings
--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,119 +1,121 @@
-$(window).load( function() {
-	$('.row3').masonry({
-	  itemSelector: '.item',
-	  columnWidth: 248
-	});
-	$('.row4').masonry({
-	  itemSelector: '.item',
-	  columnWidth: 247
-	});
-});
-
 $(document).ready(function() {
-
-$('.social a').tooltip({
-    track: true,
-    delay: 0,
-    showURL: false,
-    showBody: " - ",
-    fade: 250
-});
-
-
-    var from = $('.details .t_from'),
-        share = $('.details .t_share');
-
-  $('.social .other').click(function(){
-     $(this).parent().parent().find(from).slideToggle();
-     $(this).parent().parent().find(share).slideToggle();
-  });
-
-    $('.item').mouseleave(function() {
-        setTimeout(function() {
-            from.show();
-            share.hide();
-        }, 300)
+    $(window).load( function() {
+    	$('.row3').masonry({
+    	  itemSelector: '.item',
+    	  columnWidth: 248
+    	});
+    	$('.row4').masonry({
+    	  itemSelector: '.item',
+    	  columnWidth: 247
+    	});
     });
 
-});
-$(function() {
-    $('input, textarea').placeholder();
-});
-$('#photo_f_b').on('click', function () {
-    $('#photo').click();
-    readURL(this);
-    return false;
-});
+    $(document).ready(function() {
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+    $('.social a').tooltip({
+        track: true,
+        delay: 0,
+        showURL: false,
+        showBody: " - ",
+        fade: 250
+    });
 
-        reader.onload = function (e) {
-            $('#preview_photo').attr('src', e.target.result);
+
+        var from = $('.details .t_from'),
+            share = $('.details .t_share');
+
+      $('.social .other').click(function(){
+         $(this).parent().parent().find(from).slideToggle();
+         $(this).parent().parent().find(share).slideToggle();
+      });
+
+        $('.item').mouseleave(function() {
+            setTimeout(function() {
+                from.show();
+                share.hide();
+            }, 300)
+        });
+
+    });
+    $(function() {
+        $('input, textarea').placeholder();
+    });
+    $('#photo_f_b').on('click', function () {
+        $('#photo').click();
+        readURL(this);
+        return false;
+    });
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#preview_photo').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
         }
-
-        reader.readAsDataURL(input.files[0]);
     }
-}
 
-$(window).live('scroll', function() { return false; });
+    $(window).live('scroll', function() { return false; });
 
-$('.mini_menu').css({
-    'width':($(this).find('.mm_links').outerWidth())+'px'
-});
-
-
-$('a.signup').click(function(e) {
-    e.preventDefault();
-    $('#login').modal('hide');
-});
-
-$('a.login').click(function(e) {
-    e.preventDefault();
-    $('#signup').modal('hide');
-});
-
-$('.tabs li').click(function() {
-    $(this).addClass('active').siblings().removeClass('active');
-
-    var id = $(this).find('a').attr('href');
-    $('.tab-content > div').slideUp();
-    $(id).slideDown( function () {
+    $('.mini_menu').css({
+        'width':($(this).find('.mm_links').outerWidth())+'px'
     });
-});
 
-$(document).on('click', function() {
-        $('.drop').removeClass('active').find('ul').slideUp('fast')
-});
 
-$('.drop').on('click', function(e) {
-    $('.drop').removeClass('active').find('ul').slideUp('fast');
-    $(this).toggleClass('active').find('ul').stop().slideToggle('fast');
-    e.stopPropagation();
-});
+    $('a.signup').click(function(e) {
+        e.preventDefault();
+        $('#login').modal('hide');
+    });
 
-$('.drop').on('mouseover', function(e) {
-    $(this).addClass('active');
-});
+    $('a.login').click(function(e) {
+        e.preventDefault();
+        $('#signup').modal('hide');
+    });
 
-$('.drop').on('mouseleave', function(e) {
-    if (!$(this).find('ul:visible').length) {
-      $(this).removeClass('active');
-    }
-});
+    $('.tabs li').click(function() {
+        $(this).addClass('active').siblings().removeClass('active');
 
-$('.drop a').on('click', function() {
-    var id = $(this).attr('href');
-    $(id).modal('show');
-});
+        var id = $(this).find('a').attr('href');
+        $('.tab-content > div').slideUp();
+        $(id).slideDown( function () {
+        });
+    });
 
-$(document).ready(function() {
-    $("input[type=checkbox], input[type=radio]").uniform();
+    $(document).on('click', function() {
+            $('.drop').removeClass('active').find('ul').slideUp('fast')
+    });
 
-    $('.modal').on('shown', function() {
+    $('.drop').on('click', function(e) {
+        $('.drop').removeClass('active').find('ul').slideUp('fast');
+        $(this).toggleClass('active').find('ul').stop().slideToggle('fast');
+        e.stopPropagation();
+    });
+
+    $('.drop').on('mouseover', function(e) {
+        $(this).addClass('active');
+    });
+
+    $('.drop').on('mouseleave', function(e) {
+        if (!$(this).find('ul:visible').length) {
+          $(this).removeClass('active');
+        }
+    });
+
+    $('.drop a').on('click', function() {
+        var id = $(this).attr('href');
+        $(id).modal('show');
+    });
+
+    $(document).ready(function() {
         $("input[type=checkbox], input[type=radio]").uniform();
-    })
+
+        $('.modal').on('shown', function() {
+            $("input[type=checkbox], input[type=radio]").uniform();
+        })
+  });
 });
 
 
